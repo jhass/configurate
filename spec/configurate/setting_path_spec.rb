cc66260c2bab7bf4135eef1d5c3d360914647837
Jonne Haß <me@mrzyx.de>
2013-04-20 13:14:49 +0200

    refactor SettingPath spec

--- spec/configurate/setting_path_spec.rb
+++ spec/configurate/setting_path_spec.rb
@@ -1,34 +1,39 @@
 require 'spec_helper'
 
 describe Configurate::SettingPath do
+  let(:normal_path) { described_class.new([:foo]) }
+  let(:question_path) { described_class.new([:foo?]) }
+  let(:setter_path) { described_class.new([:foo=]) }
+  let(:long_path) { described_class.new(["foo", "bar?"]) }
+
   describe "#initialize" do
     context "with a string" do
       it "creates a path" do
-        described_class.new("foo.bar").should == described_class.new(["foo", "bar"])
+        described_class.new(long_path.to_s).should == long_path
       end
     end
   end
 
   describe "#is_question?" do
     context "with a question signature as setting" do
-      subject { described_class.new([:foo?]).is_question? }
+      subject { question_path.is_question? }
       it { should be_true }
     end
 
     context "with a normal path as setting" do
-      subject { described_class.new([:foo]).is_question? }
+      subject { normal_path.is_question? }
       it { should be_false }
     end
   end
 
   describe "#is_setter?" do
     context "with a setter signature as setting" do
-      subject { described_class.new([:foo=]).is_setter? } 
+      subject { setter_path.is_setter? } 
       it { should be_true }
     end
 
     context "with a normal path as setting" do
-      subject { described_class.new([:foo]).is_setter? }
+      subject { normal_path.is_setter? }
       it { should be_false }
     end
   end
@@ -36,18 +41,34 @@ describe Configurate::SettingPath do
 
   describe "#is_question_or_setter?" do
     context "with a question signature as setting" do
-      subject { described_class.new([:foo?]).is_question_or_setter? }
+      subject { question_path.is_question_or_setter? }
       it { should be_true }
     end
 
     context "with a setter signature as setting" do
-      subject { described_class.new([:foo=]).is_question_or_setter? } 
+      subject { setter_path.is_question_or_setter? } 
       it { should be_true }
     end
 
     context "with a normal path as setting" do
-      subject { described_class.new([:foo]).is_question_or_setter? }
+      subject { normal_path.is_question_or_setter? }
       it { should be_false }
+    end
+  end
+
+  describe "#shift" do
+    subject { question_path.shift }
+    it { should_not include "?" }
+  end
+
+  describe "#pop" do
+    subject { question_path.shift }
+    it { should_not include "?" }
+  end
+
+  describe "#each" do
+    it "should strip special characters" do
+      long_path.all? { |c| c.include? "?" }.should be_false
     end
   end
 
